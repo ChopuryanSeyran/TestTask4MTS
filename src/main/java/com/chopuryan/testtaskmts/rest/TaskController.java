e8545e149b24915b2f965466b184d877629156f9
Seyran Chopuryan <seyran.chopuryan@donriver.com>
2018-09-23 12:41:41 +0300

    fix

--- src/main/java/com/chopuryan/testtaskmts/rest/TaskController.java
+++ src/main/java/com/chopuryan/testtaskmts/rest/TaskController.java
@@ -1,5 +1,7 @@
 package com.chopuryan.testtaskmts.rest;
 
+import com.chopuryan.testtaskmts.exception.TaskNotFoundException;
+import com.chopuryan.testtaskmts.exception.WrongUuidException;
 import com.chopuryan.testtaskmts.model.Status;
 import com.chopuryan.testtaskmts.service.TaskService;
 import io.swagger.annotations.Api;
@@ -14,6 +16,7 @@ import static org.springframework.http.HttpStatus.ACCEPTED;
 import static org.springframework.http.HttpStatus.OK;
 
 @RestController
+@RequestMapping(path = "/api/")
 @Api(value = "TaskControllerAPI", produces = MediaType.APPLICATION_JSON_VALUE)
 public class TaskController {
 
@@ -28,6 +31,10 @@ public class TaskController {
             produces = MediaType.APPLICATION_JSON_VALUE)
     @ResponseStatus(ACCEPTED)
     @ResponseBody
+    @ApiOperation("Create task")
+    @ApiResponses(value = {
+            @ApiResponse(code = 202, message = "ACCEPTED", response = String.class)
+    })
     public String createTask() {
         return taskService.createTask();
     }
@@ -38,7 +45,9 @@ public class TaskController {
     @ResponseBody
     @ApiOperation("Gets the task with specific id")
     @ApiResponses(value = {
-            @ApiResponse(code = 200, message = "OK", response = Status.class)
+            @ApiResponse(code = 200, message = "OK", response = Status.class),
+            @ApiResponse(code = 400, message = "Wrong UUID"),
+            @ApiResponse(code = 404, message = "Task not found")
     })
     public Status getTask(@PathVariable String id) throws Exception {
         return taskService.getTask(id);
